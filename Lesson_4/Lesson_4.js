6cacdf342a2ffae93dd079c1775be6f9e74263f1
AleisterCrowley <vtnk21@gmail.com>
2024-03-05 22:13:02 +0200

    add changes to Homework Lesson_4

--- Lesson_4/Lesson_4.js
+++ Lesson_4/Lesson_4.js
@@ -190,10 +190,33 @@
 // sum([11,22,33])
 // console.log(sum([11,22,33]))
 
-// - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
+// - створити функцію swap(arr,index1,index2). Функція міняє місцями значення у відповідних індексах
 // Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
 
-
+// function swap(arr,index1,index2) {
+//     let arguments;
+//     arguments=arr[index1];
+//     arr[index1]=arr[index2];
+//     arr[index2]=arguments;
+//     return arr;
+// }
+// console.log(swap([11, 22, 33, 44],0,1))
 
 // - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 // Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
+
+// let money = [
+//     {currency:'USD',value:40},
+//     {currency:'EUR',value:42},
+//     {currency:'UAH',value:1},
+//     {currency:'GBP',value:48}
+// ]
+// function exchange(sumUAH,currencyValues,exchangeCurrency) {
+//     for (const moneyElement of money) {
+//         if (exchangeCurrency===moneyElement.currency) {
+//             result = sumUAH / moneyElement.value;
+//         }
+//     }
+//     return result;
+// }
+// console.log(exchange(8000,[{currency:'USD',value:40}],'USD'))
---
+++ TRAINING.js
@@ -0,0 +1,10 @@
+// const letters = ["a","b","c"];
+//
+// for (const x of letters) {
+//     console.log(x)
+// }
+
+
+for (const x of [1,2,3,4,5]){
+    console.log(x)
+}
